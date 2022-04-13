fb1d0f34c8729592677639712f676ac23249c2d4
Ben Buchanan <bsbuchanan99@gmail.com>
2022-04-13 13:47:40 -0400

    Add more rewritten drawing utils

--- include/vexes.hpp
+++ include/vexes.hpp
@@ -1,6 +1,34 @@
 #pragma once
 
 #include <ncurses.h>
+#include <string>
+#include <map>
+
+namespace vex {
+
+    // CONSTANTS
+    static const std::map<std::string, int> attributes = {
+        {"standout", A_STANDOUT},
+        {"underline", A_UNDERLINE},
+        {"reverse", A_REVERSE},
+        {"blink", A_BLINK},
+        {"dim", A_DIM},
+        {"bold", A_BOLD},
+        {"protected", A_PROTECT},
+        {"invisible", A_INVIS},
+        {"alternate", A_ALTCHARSET},
+        {"extract", A_CHARTEXT},
+        {"black", COLOR_PAIR(0)},
+        {"red", COLOR_PAIR(1)},
+        {"green", COLOR_PAIR(2)},
+        {"yellow", COLOR_PAIR(3)},
+        {"blue", COLOR_PAIR(4)},
+        {"magenta", COLOR_PAIR(5)},
+        {"cyan", COLOR_PAIR(6)},
+        {"white", COLOR_PAIR(7)}
+    };
+
+}
 
 namespace vex {
 
@@ -77,6 +105,29 @@ namespace vex {
         }
 
     public: // DRAWING FUNCTIONS
+        int getAttribute(std::string name) {
+            auto iter = attributes.find(name);
+            // Check if name exists, otherwise return A_NORMAL
+            if(iter == attributes.end()) {
+                return A_NORMAL;
+            } else {
+                return iter->second;
+            }
+        }
+        void setAttributes(int attr, WINDOW * win = NULL) {
+            if(win != NULL) {
+                wattron(win, attr);
+            } else {
+                attron(attr);
+            }
+        }
+        void unsetAttributes(int attr, WINDOW * win = NULL) {
+            if(win != NULL) {
+                wattroff(win, attr);
+            } else {
+                attroff(attr);
+            }
+        }
         void drawCharAtPoint(char ch, Vec2i p, WINDOW * win = NULL) {
             if(win != NULL) {
                 wmove(win, p.y, p.x);
@@ -85,6 +136,71 @@ namespace vex {
                 move(p.y, p.x);
                 addch(ch);
             }
+        }
+        void drawStringAtPoint(std::string text, Vec2i p, WINDOW * win = NULL) {
+            if(win != NULL) {
+                wmove(win, p.y, p.x);
+                waddstr(win, text.c_str());
+            } else {
+                move(p.y, p.x);
+                addstr(text.c_str());
+            }
+        }
+        void drawCenteredStringAtPoint(std::string text, Vec2i p, WINDOW * win = NULL) {
+            // Compute new point offset by half of string's length
+            size_t offset = text.size() / 2;
+            Vec2i newPoint(p.x - offset, p.y);
+            
+            // Delegate to drawStringAtPoint with new point
+            drawStringAtPoint(text, newPoint, win);
+        }
+        void drawCustomHLineBetweenPoints(char ch, Vec2i a, Vec2i b, WINDOW * win = NULL) {
+            // Only draw line if points are on the same Y level
+            if(a.y != b.y) { return; }
+
+            // To avoid issues of order, we check which x is larger
+            if(a.x < b.x) {
+                for(int i = a.x; i < b.x + 1; i++) {
+                    drawCharAtPoint(ch, {i, a.y}, win);
+                }
+            } else if(a.x > b.x) {
+                for(int i = b.x; i < a.x + 1; i++) {
+                    drawCharAtPoint(ch, {i, b.y}, win);
+                }
+            } else {
+                // x is equal, so we only draw a single character
+                drawCharAtPoint(ch, a, win);
+            }
+        }
+        void drawHLineBetweenPoints(Vec2i a, Vec2i b, WINDOW * win = NULL) {
+            // Delegate to the custom line with the HLINE character
+            setAttributes(getAttribute("alternate"), win);
+            drawCustomHLineBetweenPoints(ACS_HLINE, a, b, win);
+            unsetAttributes(getAttribute("alternate"), win);
+        }
+        void drawCustomVLineBetweenPoints(char ch, Vec2i a, Vec2i b, WINDOW * win = NULL) {
+            // Only draw line if points are on the same X level
+            if(a.x != b.x) { return; }
+
+            // To avoid issues of order, we check which y is larger
+            if(a.y < b.y) {
+                for(int i = a.y; i < b.y + 1; i++) {
+                    drawCharAtPoint(ch, {a.x, i}, win);
+                }
+            } else if(a.y > b.y) {
+                for(int i = b.y; i < a.y + 1; i++) {
+                    drawCharAtPoint(ch, {b.x, i}, win);
+                }
+            } else {
+                // y is equal, so we only draw a single character
+                drawCharAtPoint(ch, a, win);
+            }
+        }
+        void drawVLineBetweenPoints(Vec2i a, Vec2i b, WINDOW * win = NULL) {
+            // Delegate to the custom line with the VLINE character
+            setAttributes(getAttribute("alternate"), win);
+            drawCustomVLineBetweenPoints(ACS_VLINE, a, b, win);
+            unsetAttributes(getAttribute("alternate"), win);
         }
 
     };
--- src/demo1.cpp
+++ src/demo1.cpp
@@ -7,7 +7,21 @@ public:
         vex::Vec2i center = getMidpoint();
         drawCharAtPoint('#', center);
 
-        // TODO: Mimic the old demo1 file
+        vex::Vec2i p1(center.x, center.y - 4);
+        vex::Vec2i p2(center.x, center.y - 2);
+        drawStringAtPoint("This string is not centered.", p1);
+        drawCenteredStringAtPoint("This string is centered!", p2);
+
+        vex::Vec2i a1(3, 3);
+        vex::Vec2i b1(9, 3);
+        drawHLineBetweenPoints(a1, b1);
+        vex::Vec2i a2(3, 5);
+        vex::Vec2i b2(3, 11);
+        drawVLineBetweenPoints(a2, b2);
+        vex::Vec2i p3(11, 3);
+        vex::Vec2i p4(5, 8);
+        drawStringAtPoint("We have HLines", p3);
+        drawStringAtPoint("And VLines!", p4);
     }
 
     void run() override {
