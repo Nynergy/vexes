a4ed9fa084a70de5d1f5826c3e256fe074dc107e
Ben Buchanan <bsbuchanan99@gmail.com>
2022-04-15 09:55:26 -0400

    Refactor drawing into Renderable classes

--- include/vexes.hpp
+++ include/vexes.hpp
@@ -4,9 +4,9 @@
 #include <string>
 #include <map>
 
+// CONSTANTS
 namespace vex {
 
-    // CONSTANTS
     static const std::map<std::string, int> attributes = {
         {"standout", A_STANDOUT},
         {"underline", A_UNDERLINE},
@@ -30,9 +30,9 @@ namespace vex {
 
 }
 
+// STRUCTS
 namespace vex {
 
-    // STRUCTS
     template <typename T>
     struct Vec2 {
         T x, y;
@@ -85,190 +85,280 @@ namespace vex {
 
 }
 
+// BASE CLASS DEFINITIONS
 namespace vex {
 
-    // BASE CLASSES
+    // DECLARATIONS
+    class Renderable;
+    class Engine;
+
+    // DEFINITIONS
+    class Renderable {
+
+    protected:
+        Vec2i pos;
+
+    public:
+        Renderable(Vec2i pos_);
+
+        virtual void draw(Engine& engine) = 0;
+
+    };
+
     class Engine {
 
     protected:
-        void initializeScreenVariables() {
-            initscr();		        // Begin curses mode
-            cbreak();		        // Disable line buffering
-            keypad(stdscr, TRUE);	// Enable extra keys
-            noecho();		        // Disable echoing keys to console
-            start_color();		    // Enable color mode
-            curs_set(0);		    // Set cursor to be invisible
-            timeout(50);		    // Make getch a non-blocking call
-        }
-        void initializeColorPairs() {
-            int backgroundColor = -1; // Transparency
-            use_default_colors();
-
-            init_pair(0, COLOR_BLACK,   backgroundColor);
-            init_pair(1, COLOR_RED,     backgroundColor);
-            init_pair(2, COLOR_GREEN,   backgroundColor);
-            init_pair(3, COLOR_YELLOW,  backgroundColor);
-            init_pair(4, COLOR_BLUE,    backgroundColor);
-            init_pair(5, COLOR_MAGENTA, backgroundColor);
-            init_pair(6, COLOR_CYAN,    backgroundColor);
-            init_pair(7, COLOR_WHITE,   backgroundColor);
-        }
-        void setupCursesEnvironment() {
-            initializeScreenVariables();
-            initializeColorPairs();
-        }
-        void teardownCursesEnvironment() {
-            endwin();
-        }
+        void initializeScreenVariables();
+        void initializeColorPairs();
+        void setupCursesEnvironment();
+        void teardownCursesEnvironment();
 
     public:
-        Engine() { setupCursesEnvironment(); }
-        virtual ~Engine() { teardownCursesEnvironment(); }
+        Engine();
+        virtual ~Engine();
 
         virtual void init() = 0;
         virtual void run() = 0;
 
-    public: // UTILITY FUNCTIONS
-        Vec2i getMidpoint() {
-            return {COLS / 2, LINES / 2};
-        }
+        void draw(Renderable& obj);
 
-    public: // DRAWING FUNCTIONS
-        int getAttribute(std::string name) {
-            auto iter = attributes.find(name);
-            // Check if name exists, otherwise return A_NORMAL
-            if(iter == attributes.end()) {
-                return A_NORMAL;
-            } else {
-                return iter->second;
-            }
-        }
-        void setAttributes(int attr, WINDOW * win = NULL) {
-            if(win != NULL) {
-                wattron(win, attr);
-            } else {
-                attron(attr);
-            }
-        }
-        void unsetAttributes(int attr, WINDOW * win = NULL) {
-            if(win != NULL) {
-                wattroff(win, attr);
-            } else {
-                attroff(attr);
-            }
-        }
-        int combineAttributes(int num, ...) {
-            va_list argList;
-            int attr = A_NORMAL;
-            va_start(argList, num);
+    public: // UTILITY METHODS
+        Vec2i getMidpoint();
 
-            for(int i = 0; i < num; i++) {
-                attr |= va_arg(argList, int);
-            }
+    public: // DRAWING METHODS
+        int getAttribute(std::string name);
+        void setAttributes(int attr, WINDOW * win = NULL);
+        void unsetAttributes(int attr, WINDOW * win = NULL);
+        int combineAttributes(int num, ...);
 
-            va_end(argList);
-            return attr;
-        }
-        void drawCharAtPoint(char ch, Vec2i p, WINDOW * win = NULL) {
-            if(win != NULL) {
-                wmove(win, p.y, p.x);
-                waddch(win, ch);
-            } else {
-                move(p.y, p.x);
-                addch(ch);
-            }
-        }
-        void drawStringAtPoint(std::string text, Vec2i p, WINDOW * win = NULL) {
-            if(win != NULL) {
-                wmove(win, p.y, p.x);
-                waddstr(win, text.c_str());
-            } else {
-                move(p.y, p.x);
-                addstr(text.c_str());
-            }
-        }
-        void drawCenteredStringAtPoint(std::string text, Vec2i p, WINDOW * win = NULL) {
-            // Compute new point offset by half of string's length
-            size_t offset = text.size() / 2;
-            Vec2i newPoint(p.x - offset, p.y);
-            
-            // Delegate to drawStringAtPoint with new point
-            drawStringAtPoint(text, newPoint, win);
-        }
-        void drawCustomHLineBetweenPoints(char ch, Vec2i a, Vec2i b, WINDOW * win = NULL) {
-            // Only draw line if points are on the same Y level
-            if(a.y != b.y) { return; }
+        void drawCharAtPoint(char ch, Vec2i p, WINDOW * win = NULL);
+        void drawStringAtPoint(std::string text, Vec2i p, WINDOW * win = NULL);
+        void drawCenteredStringAtPoint(std::string text, Vec2i p, WINDOW * win = NULL);
 
-            // To avoid issues of order, we check which x is larger
-            if(a.x < b.x) {
-                for(int i = a.x; i < b.x + 1; i++) {
-                    drawCharAtPoint(ch, {i, a.y}, win);
-                }
-            } else if(a.x > b.x) {
-                for(int i = b.x; i < a.x + 1; i++) {
-                    drawCharAtPoint(ch, {i, b.y}, win);
-                }
-            } else {
-                // x is equal, so we only draw a single character
-                drawCharAtPoint(ch, a, win);
-            }
-        }
-        void drawHLineBetweenPoints(Vec2i a, Vec2i b, WINDOW * win = NULL) {
-            // Delegate to the custom line with the HLINE character
-            setAttributes(getAttribute("alternate"), win);
-            drawCustomHLineBetweenPoints(ACS_HLINE, a, b, win);
-            unsetAttributes(getAttribute("alternate"), win);
-        }
-        void drawCustomVLineBetweenPoints(char ch, Vec2i a, Vec2i b, WINDOW * win = NULL) {
-            // Only draw line if points are on the same X level
-            if(a.x != b.x) { return; }
+        void drawCustomHLineBetweenPoints(char ch, Vec2i a, Vec2i b, WINDOW * win = NULL);
+        void drawHLineBetweenPoints(Vec2i a, Vec2i b, WINDOW * win = NULL);
+        void drawCustomVLineBetweenPoints(char ch, Vec2i a, Vec2i b, WINDOW * win = NULL);
+        void drawVLineBetweenPoints(Vec2i a, Vec2i b, WINDOW * win = NULL);
 
-            // To avoid issues of order, we check which y is larger
-            if(a.y < b.y) {
-                for(int i = a.y; i < b.y + 1; i++) {
-                    drawCharAtPoint(ch, {a.x, i}, win);
-                }
-            } else if(a.y > b.y) {
-                for(int i = b.y; i < a.y + 1; i++) {
-                    drawCharAtPoint(ch, {b.x, i}, win);
-                }
-            } else {
-                // y is equal, so we only draw a single character
-                drawCharAtPoint(ch, a, win);
-            }
-        }
-        void drawVLineBetweenPoints(Vec2i a, Vec2i b, WINDOW * win = NULL) {
-            // Delegate to the custom line with the VLINE character
-            setAttributes(getAttribute("alternate"), win);
-            drawCustomVLineBetweenPoints(ACS_VLINE, a, b, win);
-            unsetAttributes(getAttribute("alternate"), win);
-        }
-        void drawCustomBox(IntRect b, char * chars, WINDOW * win = NULL) {
-            // Draw top and bottom of box
-            drawCustomHLineBetweenPoints(chars[0], b.ul(), b.ur(), win);
-            drawCustomHLineBetweenPoints(chars[1], b.ll(), b.lr(), win);
-
-            // Draw left and right of box
-            drawCustomVLineBetweenPoints(chars[2], b.ul(), b.ll(), win);
-            drawCustomVLineBetweenPoints(chars[3], b.ur(), b.lr(), win);
-
-            // Draw corners of the box
-            drawCharAtPoint(chars[4], b.ul(), win);
-            drawCharAtPoint(chars[5], b.ur(), win);
-            drawCharAtPoint(chars[6], b.ll(), win);
-            drawCharAtPoint(chars[7], b.lr(), win);
-        }
-        void drawBox(IntRect b, WINDOW * win = NULL) {
-            // Define alternate characters to use and set attribute on
-            char alts[] = {
-                            ACS_HLINE, ACS_HLINE, ACS_VLINE, ACS_VLINE,
-                            ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER
-                          };
-            setAttributes(getAttribute("alternate"), win);
-            drawCustomBox(b, alts, win);
-            unsetAttributes(getAttribute("alternate"), win);
-        }
+        void drawCustomBox(IntRect b, char * chars, WINDOW * win = NULL);
+        void drawBox(IntRect b, WINDOW * win = NULL);
 
     };
+
+}
+
+// RENDERABLE SUBCLASSES
+namespace vex {
+
+    class Text : public Renderable {
+
+    private:
+        std::string text;
+        int attr = A_NORMAL;
+
+    public:
+        Text(std::string text_, Vec2i pos_);
+
+        virtual void draw(Engine& engine) override;
+        void setAttributes(int attr_);
+
+    };
+
+}
+
+// IMPLEMENTATIONS
+namespace vex {
+
+    // RENDERABLES
+    Renderable::Renderable(Vec2i pos_): pos(pos_) {}
+
+    Text::Text(std::string text_, Vec2i pos_): Renderable(pos_), text(text_) {}
+    void Text::draw(Engine& engine) {
+        engine.setAttributes(attr);
+        engine.drawStringAtPoint(text, pos);
+        engine.unsetAttributes(attr);
+    }
+    void Text::setAttributes(int attr_) {
+        attr = attr_;
+    }
+
+    // ENGINE BASE CLASS
+    Engine::Engine() { setupCursesEnvironment(); }
+    Engine::~Engine() { teardownCursesEnvironment(); }
+
+    // ENVIRONMENT SETUP AND TEARDOWN
+    void Engine::initializeScreenVariables() {
+        initscr();		        // Begin curses mode
+        cbreak();		        // Disable line buffering
+        keypad(stdscr, TRUE);	// Enable extra keys
+        noecho();		        // Disable echoing keys to console
+        start_color();		    // Enable color mode
+        curs_set(0);		    // Set cursor to be invisible
+        timeout(50);		    // Make getch a non-blocking call
+    }
+    void Engine::initializeColorPairs() {
+        int backgroundColor = -1; // Transparency
+        use_default_colors();
+
+        init_pair(0, COLOR_BLACK,   backgroundColor);
+        init_pair(1, COLOR_RED,     backgroundColor);
+        init_pair(2, COLOR_GREEN,   backgroundColor);
+        init_pair(3, COLOR_YELLOW,  backgroundColor);
+        init_pair(4, COLOR_BLUE,    backgroundColor);
+        init_pair(5, COLOR_MAGENTA, backgroundColor);
+        init_pair(6, COLOR_CYAN,    backgroundColor);
+        init_pair(7, COLOR_WHITE,   backgroundColor);
+    }
+    void Engine::setupCursesEnvironment() {
+        initializeScreenVariables();
+        initializeColorPairs();
+    }
+    void Engine::teardownCursesEnvironment() {
+        endwin();
+    }
+
+    // UTILITY METHODS
+    Vec2i Engine::getMidpoint() {
+        return {COLS / 2, LINES / 2};
+    }
+
+    // DRAWING METHODS
+    void Engine::draw(Renderable& obj) {
+        obj.draw(*this);
+    }
+    int Engine::getAttribute(std::string name) {
+        auto iter = attributes.find(name);
+        // Check if name exists, otherwise return A_NORMAL
+        if(iter == attributes.end()) {
+            return A_NORMAL;
+        } else {
+            return iter->second;
+        }
+    }
+    void Engine::setAttributes(int attr, WINDOW * win) {
+        if(win != NULL) {
+            wattron(win, attr);
+        } else {
+            attron(attr);
+        }
+    }
+    void Engine::unsetAttributes(int attr, WINDOW * win) {
+        if(win != NULL) {
+            wattroff(win, attr);
+        } else {
+            attroff(attr);
+        }
+    }
+    int Engine::combineAttributes(int num, ...) {
+        va_list argList;
+        int attr = A_NORMAL;
+        va_start(argList, num);
+
+        for(int i = 0; i < num; i++) {
+            attr |= va_arg(argList, int);
+        }
+
+        va_end(argList);
+        return attr;
+    }
+    void Engine::drawCharAtPoint(char ch, Vec2i p, WINDOW * win) {
+        if(win != NULL) {
+            wmove(win, p.y, p.x);
+            waddch(win, ch);
+        } else {
+            move(p.y, p.x);
+            addch(ch);
+        }
+    }
+    void Engine::drawStringAtPoint(std::string text, Vec2i p, WINDOW * win) {
+        if(win != NULL) {
+            wmove(win, p.y, p.x);
+            waddstr(win, text.c_str());
+        } else {
+            move(p.y, p.x);
+            addstr(text.c_str());
+        }
+    }
+    void Engine::drawCenteredStringAtPoint(std::string text, Vec2i p, WINDOW * win) {
+        // Compute new point offset by half of string's length
+        size_t offset = text.size() / 2;
+        Vec2i newPoint(p.x - offset, p.y);
+        
+        // Delegate to drawStringAtPoint with new point
+        drawStringAtPoint(text, newPoint, win);
+    }
+    void Engine::drawCustomHLineBetweenPoints(char ch, Vec2i a, Vec2i b, WINDOW * win) {
+        // Only draw line if points are on the same Y level
+        if(a.y != b.y) { return; }
+
+        // To avoid issues of order, we check which x is larger
+        if(a.x < b.x) {
+            for(int i = a.x; i < b.x + 1; i++) {
+                drawCharAtPoint(ch, {i, a.y}, win);
+            }
+        } else if(a.x > b.x) {
+            for(int i = b.x; i < a.x + 1; i++) {
+                drawCharAtPoint(ch, {i, b.y}, win);
+            }
+        } else {
+            // x is equal, so we only draw a single character
+            drawCharAtPoint(ch, a, win);
+        }
+    }
+    void Engine::drawHLineBetweenPoints(Vec2i a, Vec2i b, WINDOW * win) {
+        // Delegate to the custom line with the HLINE character
+        setAttributes(getAttribute("alternate"), win);
+        drawCustomHLineBetweenPoints(ACS_HLINE, a, b, win);
+        unsetAttributes(getAttribute("alternate"), win);
+    }
+    void Engine::drawCustomVLineBetweenPoints(char ch, Vec2i a, Vec2i b, WINDOW * win) {
+        // Only draw line if points are on the same X level
+        if(a.x != b.x) { return; }
+
+        // To avoid issues of order, we check which y is larger
+        if(a.y < b.y) {
+            for(int i = a.y; i < b.y + 1; i++) {
+                drawCharAtPoint(ch, {a.x, i}, win);
+            }
+        } else if(a.y > b.y) {
+            for(int i = b.y; i < a.y + 1; i++) {
+                drawCharAtPoint(ch, {b.x, i}, win);
+            }
+        } else {
+            // y is equal, so we only draw a single character
+            drawCharAtPoint(ch, a, win);
+        }
+    }
+    void Engine::drawVLineBetweenPoints(Vec2i a, Vec2i b, WINDOW * win) {
+        // Delegate to the custom line with the VLINE character
+        setAttributes(getAttribute("alternate"), win);
+        drawCustomVLineBetweenPoints(ACS_VLINE, a, b, win);
+        unsetAttributes(getAttribute("alternate"), win);
+    }
+    void Engine::drawCustomBox(IntRect b, char * chars, WINDOW * win) {
+        // Draw top and bottom of box
+        drawCustomHLineBetweenPoints(chars[0], b.ul(), b.ur(), win);
+        drawCustomHLineBetweenPoints(chars[1], b.ll(), b.lr(), win);
+
+        // Draw left and right of box
+        drawCustomVLineBetweenPoints(chars[2], b.ul(), b.ll(), win);
+        drawCustomVLineBetweenPoints(chars[3], b.ur(), b.lr(), win);
+
+        // Draw corners of the box
+        drawCharAtPoint(chars[4], b.ul(), win);
+        drawCharAtPoint(chars[5], b.ur(), win);
+        drawCharAtPoint(chars[6], b.ll(), win);
+        drawCharAtPoint(chars[7], b.lr(), win);
+    }
+    void Engine::drawBox(IntRect b, WINDOW * win) {
+        // Define alternate characters to use and set attribute on
+        char alts[] = {
+                        ACS_HLINE, ACS_HLINE, ACS_VLINE, ACS_VLINE,
+                        ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER
+                      };
+        setAttributes(getAttribute("alternate"), win);
+        drawCustomBox(b, alts, win);
+        unsetAttributes(getAttribute("alternate"), win);
+    }
 
 }
--- src/demo1.cpp
+++ src/demo1.cpp
@@ -4,46 +4,16 @@ class DemoEngine : public vex::Engine {
 
 public:
     void init() override {
-        vex::Vec2i center = getMidpoint();
-        drawCharAtPoint('#', center);
-
-        vex::Vec2i p1(center.x, center.y - 4);
-        vex::Vec2i p2(center.x, center.y - 2);
-        drawStringAtPoint("This string is not centered.", p1);
-        drawCenteredStringAtPoint("This string is centered!", p2);
-
-        vex::Vec2i a1(3, 3);
-        vex::Vec2i b1(9, 3);
-        drawHLineBetweenPoints(a1, b1);
-        vex::Vec2i a2(3, 5);
-        vex::Vec2i b2(3, 11);
-        drawVLineBetweenPoints(a2, b2);
-        vex::Vec2i p3(11, 3);
-        vex::Vec2i p4(5, 8);
-        drawStringAtPoint("We have HLines", p3);
-        drawStringAtPoint("And VLines!", p4);
-
-        vex::Vec2i box_corner(10, 10);
-        vex::Vec2i box_dimensions(10, 10);
-        vex::IntRect box(box_corner, box_dimensions);
-        drawBox(box);
-        vex::Vec2i p5(15, 14);
-        vex::Vec2i p6(15, 15);
-        drawCenteredStringAtPoint("Boxes", p5);
-        drawCenteredStringAtPoint("too", p6);
-
-        setAttributes(getAttribute("green"));
-        vex::Vec2i init_p(0, 0);
-        drawStringAtPoint("Engine initialized successfully!", init_p);
-        unsetAttributes(getAttribute("green"));
+        vex::Text init_txt("Engine initialized successfully!", {0, 0});
+        init_txt.setAttributes(getAttribute("green"));
+        draw(init_txt);
 
         int quit_attr = combineAttributes(3, getAttribute("red"),
                                              getAttribute("reverse"),
                                              getAttribute("bold"));
-        setAttributes(quit_attr);
-        vex::Vec2i quit_p(0, 1);
-        drawStringAtPoint("Press Q to quit", quit_p);
-        unsetAttributes(quit_attr);
+        vex::Text quit_txt("Press Q to quit.", {0, 1});
+        quit_txt.setAttributes(quit_attr);
+        draw(quit_txt);
     }
 
     void run() override {
