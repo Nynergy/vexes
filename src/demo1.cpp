ce7d9d76ccf220dfb4c874aa257a49c6e9a06f8e
Ben Buchanan <bsbuchanan99@gmail.com>
2022-04-18 08:18:13 -0400

    Refactor run loop

--- src/demo1.cpp
+++ src/demo1.cpp
@@ -3,6 +3,9 @@
 class DemoEngine : public vex::Engine {
 
 private:
+    int key;
+    bool userQuit = false;
+    vex::Text * fps;
     std::vector<vex::Renderable *> renderables;
 
 public:
@@ -12,31 +15,19 @@ public:
         }
     }
 
-    // TODO: Push Renderables into an array, iterated over in the run() method
     void init() override {
-        // vex::Text
-        vex::Text * init_txt = new vex::Text("Engine initialized successfully!", {0, LINES - 2});
-        init_txt->setAttributes(getAttribute("green"));
-        renderables.push_back(init_txt);
-
-        int quit_attr = combineAttributes(3, getAttribute("red"),
-                                             getAttribute("reverse"),
-                                             getAttribute("bold"));
-        vex::Text * quit_txt = new vex::Text("Press Q to quit.", {0, LINES - 1});
-        quit_txt->setAttributes(quit_attr);
-        renderables.push_back(quit_txt);
-
+        // vex::Glyph
         vex::Vec2i midpoint = getMidpoint();
+        vex::Glyph * mid_glyph = new vex::Glyph('#', midpoint);
+        renderables.push_back(mid_glyph);
+
+        // vex::Text
         vex::Text * reg_txt = new vex::Text("This string is not centered.", {midpoint.x, midpoint.y - 4});
         renderables.push_back(reg_txt);
 
         vex::Text * center_txt = new vex::Text("This string is centered!", {midpoint.x, midpoint.y - 2});
         center_txt->setCentered(true);
         renderables.push_back(center_txt);
-
-        // vex::Glyph
-        vex::Glyph * mid_glyph = new vex::Glyph('#', midpoint);
-        renderables.push_back(mid_glyph);
 
         // vex::Line
         vex::HLine * h_line = new vex::HLine({3, 3}, {9, 3});
@@ -62,20 +53,46 @@ public:
         // TODO: vex::Border
         // TODO: vex::Circle
         // TODO: vex::Ellipse
+
+        vex::Text * init_txt = new vex::Text("Engine initialized successfully!", {0, LINES - 2});
+        init_txt->setAttributes(getAttribute("green"));
+        renderables.push_back(init_txt);
+
+        int quit_attr = combineAttributes(3, getAttribute("red"),
+                                             getAttribute("reverse"),
+                                             getAttribute("bold"));
+        vex::Text * quit_txt = new vex::Text("Press Q to quit.", {0, LINES - 1});
+        quit_txt->setAttributes(quit_attr);
+        renderables.push_back(quit_txt);
+
+        fps = new vex::Text("", {0, 0});
+        renderables.push_back(fps);
     }
 
-    // TODO: Turn into input -> update -> render loop
     void run() override {
-        int key;
-        vex::Text * fps = new vex::Text("", {0, 0});
-        renderables.push_back(fps);
-        while((key = getch()) != 'q') {
-            elapsedTime = clock.getElapsedTime(true);
-            fps->setText("FPS: " + std::to_string(1.0 / elapsedTime));
+        while(!userQuit) {
+            handleInput();
+            update();
+            render();
+        }
+    }
 
-            for(auto& obj : renderables) {
-                draw(*obj);
-            }
+    void handleInput() {
+        key = getch();
+
+        if(key == 'q') {
+            userQuit = true;
+        }
+    }
+
+    void update() {
+        elapsedTime = clock.getElapsedTime(true);
+        fps->setText("FPS: " + std::to_string(1.0 / elapsedTime));
+    }
+
+    void render() {
+        for(auto& obj : renderables) {
+            draw(*obj);
         }
     }
 
